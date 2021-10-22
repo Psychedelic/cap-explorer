543bfacdbc1782677f709ac91bd41def9c1ce25e
Helder Oliveira <heldrida@gmail.com>
2021-10-22 19:18:19 +0100

    refactor: 💡 update the generate random principal, as ts and browser use

--- packages/generate-random-principal/index.js
+++
@@ -1,7 +0,0 @@
-const { Ed25519KeyIdentity } = require('@dfinity/identity');
-
-const identity = Ed25519KeyIdentity.generate();
-
-const principal = identity.getPrincipal();
-
-process.stdout.write(principal.toText());
