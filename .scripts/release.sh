7549b813cf78047883c50b6599dc0ed5bab1fd70
Helder Oliveira <heldrida@gmail.com>
2021-11-18 19:59:20 +0000

    refactor: 💡 push src and tags separately

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -39,6 +39,12 @@ git add --force --ignore-errors packages/dashboard/package.json packages/dashboa
 
 git show --summary
 
-# git push --follow-tags origin "$BRANCH"
+git push --follow-tags origin "$BRANCH"
 
-git push --atomic origin "$BRANCH" "v$VERSION"
+# git push --atomic origin "$BRANCH" "v$VERSION"
+
+echo "The VERSION is"
+echo "$VERSION"
+
+echo "The tags are"
+git tag
